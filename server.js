245729a940eb576316b2f4ff0d1ee8f6fd9ce8e5
Axel <axosar@gmail.com>
2019-05-23 15:54:26 -0500

    DB posgreSQL
    Setting the database

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'IMCYC',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+db.select('*').from('users').then(data => {
+  console.log(data);
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -43,18 +58,17 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const {email,name,password} = req.body;
-  bcrypt.hash(password, null, null, function(err, hash) {
-    console.log(hash);
-  });
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date()
-  });
-  res.json(database.users[database.users.length-1]);
+  db('users')
+    .returning('*')
+    .insert({
+      email: email,
+      name: name,
+      joined: new Date()
+    })
+    .then(user => {
+      res.json(user[0]);
+    })
+    .catch(err => res.status(400).json('Unable to register!'))
 })
 
 app.get('/profile/:id', (req, res) => {
