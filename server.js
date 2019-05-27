2b8d709f4cc5697899417d15f354106483365af7
Axel <axosar@gmail.com>
2019-05-27 15:47:49 -0500

    changing response

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-  res.send(database.users)
+  res.send('it is working!')
 })
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
