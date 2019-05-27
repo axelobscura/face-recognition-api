38634071cf944d691ca7d8e893494ad4df0e7fcd
Axel <axosar@gmail.com>
2019-05-27 16:36:16 -0500

    testing

--- server.js
+++ server.js
@@ -48,9 +48,7 @@ const database = {
   ]
 }
 
-app.get('/', (req, res) => {
-  res.send('it is working!')
-})
+app.get('/', (req, res) => { res.send('it is working!') })
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
